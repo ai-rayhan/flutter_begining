034983bb4c2fe8630aac11ccc40f838ea9a11cfe
RayhanAhmed <rayhanahmed1127@gmail.com>
2023-09-08 23:23:19 +0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Profile"),centerTitle: true,),
+      body: const SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+          Icon(Icons.account_circle_rounded,color: Colors.green,size: 50,),
+          Text("Rayhan Ahmed",style: TextStyle(color: Colors.green,fontSize: 24),),
+          Text("Flutter Batch 4",style: TextStyle(color: Colors.blue,fontSize: 20),),
+        ],),
+      ),
+    );
+  }
+}
