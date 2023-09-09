2bd87d37b5e3c00149b60c4a1a16d9ebb9a006ae
RayhanAhmed <rayhanahmed1127@gmail.com>
2023-09-09 07:13:35 +0600

    Assignment

--- lib/main.dart
+++ lib/main.dart
@@ -14,28 +14,60 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: const HomeScreen(),
     );
   }
 }
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Profile"),centerTitle: true,),
-      body: const SizedBox(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        elevation: 70,
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        title: const Text("Home"),
+        leading: const Icon(Icons.storefront),
+        actions: const [
+           Icon(Icons.search),
+        ],
+      ),
+      body: SizedBox(
         height: double.infinity,
         width: double.infinity,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-          Icon(Icons.account_circle_rounded,color: Colors.green,size: 50,),
-          Text("Rayhan Ahmed",style: TextStyle(color: Colors.green,fontSize: 24),),
-          Text("Flutter Batch 4",style: TextStyle(color: Colors.blue,fontSize: 20),),
-        ],),
+            const Text(
+              "This is mod 5 Assignment",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+            ),
+            RichText(
+                text: const TextSpan(
+              text: "My ",
+              style: TextStyle(color: Colors.red, fontSize: 20),
+              children: [
+                TextSpan(
+                  text: "phone ",
+                  style: TextStyle(color: Colors.blue, fontSize: 12),
+                ),
+                TextSpan(
+                  text: "name ",
+                  style: TextStyle(color: Colors.purpleAccent, fontSize: 16),
+                ),
+                TextSpan(
+                  text: "Sumsumg Galaxy J7",
+                  style: TextStyle(color: Colors.orange, fontSize: 20),
+                )
+              ],
+            ))
+          ],
+        ),
       ),
     );
   }
